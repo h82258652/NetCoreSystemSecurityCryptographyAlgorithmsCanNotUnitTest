0f616d692bfdc2ecd721a382a6349fbe4b8f11dd
h82258652 <h82258652@hotmail.com>
2016-12-09 23:32:23 +0800

    upload demo

---
+++ Solution/Library/HashHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Library
+{
+    public static class HashHelper
+    {
+        public static string GetStringMD5(string str)
+        {
+            using (var md5 = MD5.Create())
+            {
+                var bytes = md5.ComputeHash(Encoding.UTF8.GetBytes(str));
+                return BitConverter.ToString(bytes).Replace("-", "");
+            }
+        }
+    }
+}
---
+++ Solution/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Library;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            HashHelper.GetStringMD5("hello world");
+        }
+    }
+}
